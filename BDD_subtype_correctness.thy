e66d497b81397f225d0985b9f6850d1fc6bb691f
Jan-Willem Maessen <jmaessen@fb.com>
2021-11-03 22:28:57 -0400

    It's not correctness, it's completeness.  State it independently of erasures.

--- BDD_subtype_correctness.thy
+++ BDD_subtype_correctness.thy
@@ -319,7 +319,7 @@ lemma common_dom_e:
   using pwfbdd.cases apply fastforce
   done
 
-theorem common_correct_a:
+theorem common_complete_a:
   "erasures c tr s t e \<Longrightarrow>
    well_formed_tr c tr \<Longrightarrow> n \<le> c \<Longrightarrow>
    disj_free c tr t \<Longrightarrow> sub_free c tr e \<Longrightarrow>
@@ -465,215 +465,17 @@ theorem common_correct_a:
   apply (metis BDD.distinct(3) BDD.distinct(5) BDD.inject order.strict_implies_order pwf_weaken pwfbdd.simps)
   done
 
-theorem common_correct_t0:
-  "well_formed_tr c tr \<Longrightarrow> n \<le> c \<Longrightarrow> t \<noteq> e \<Longrightarrow>
-   disj_free c tr t \<Longrightarrow> sub_free c tr e \<Longrightarrow>
-   pwfbdd c tr t \<Longrightarrow> pwfbdd c tr e \<Longrightarrow>
-   pwfbdd (Suc n) tr t \<Longrightarrow> pwfbdd (Suc n) tr e \<Longrightarrow>
-   common n c tr t e = Some s \<Longrightarrow>
-   (erase_disjoints c tr s = t \<and> erase_subtypes c tr s = e)"
-  apply (induction n c tr t e rule: common.induct)
-  apply (simp_all del: erase_disjoints.simps erase_subtypes.simps split: option.split)
-  apply simp_all
-  defer
-  (* Case 5 / 5 *)
-  apply (case_tac "tr it c")
-  apply (auto simp add: select'_def split: option.split)[3]
-  sledgehammer
-  apply (subgoal_tac "erase_disjoints c tr x2 = et")
-  prefer 2
-  apply (metis BDD.distinct(3) BDD.distinct(5) BDD.inject disj_free.cases less_Suc_eq_le pwf_weaken pwfbdd.simps)
-  apply (smt (verit, ccfv_SIG) BDD.distinct(3) BDD.distinct(5) BDD.inject BDD_select.select_def erase_disj_df_noop erase_disj_disj_free erase_disjoints.simps(1) leI less_trans nat_neq_iff pwf_is_norm pwf_weaken pwfbdd.cases)
-  apply (metis BDD.distinct(3) BDD.distinct(5) BDD.inject disj_free.simps)
-  apply (subgoal_tac "erase_disjoints c tr x2 = tt")
-  prefer 2
-  apply (metis BDD.distinct(3) BDD.distinct(5) BDD.inject disj_free.cases less_Suc_eq_le pwf_weaken pwfbdd.simps)
-  apply (subgoal_tac "erase_disjoints c tr x2a = et")
-  prefer 2
-  apply (metis BDD.distinct(3) BDD.distinct(5) BDD.inject disj_free.cases less_Suc_eq_le pwf_weaken pwfbdd.simps)
-  apply (smt (verit, ccfv_SIG) BDD.distinct(3) BDD.distinct(5) BDD.inject BDD_select.select_def erase_disj_df_noop erase_disj_disj_free erase_disjoints.simps(1) leI less_trans nat_neq_iff pwf_is_norm pwf_weaken pwfbdd.cases)
-  (* Case 4 / 5 *)
-  apply (case_tac "tr it c")
-  apply (auto simp add: select'_def split: option.split)[3]
-  apply (subgoal_tac "erase_disjoints c tr x2 = et")
-  prefer 2
-  apply (metis BDD.distinct(3) BDD.distinct(5) BDD.inject disj_free.cases less_Suc_eq_le pwf_weaken pwfbdd.simps)
-  apply (smt (verit, ccfv_SIG) BDD.distinct(3) BDD.distinct(5) BDD.inject BDD_select.select_def erase_disj_df_noop erase_disj_disj_free erase_disjoints.simps(1) leI less_trans nat_neq_iff pwf_is_norm pwf_weaken pwfbdd.cases)
-  apply (metis BDD.distinct(3) BDD.distinct(5) BDD.inject disj_free.simps)
-  apply (subgoal_tac "erase_disjoints c tr x2 = tt")
-  prefer 2
-  apply (metis BDD.distinct(3) BDD.distinct(5) BDD.inject disj_free.cases less_Suc_eq_le pwf_weaken pwfbdd.simps)
-  apply (subgoal_tac "erase_disjoints c tr x2a = et")
-  prefer 2
-  apply (metis BDD.distinct(3) BDD.distinct(5) BDD.inject disj_free.cases less_Suc_eq_le pwf_weaken pwfbdd.simps)
-  apply (smt (verit, ccfv_SIG) BDD.distinct(3) BDD.distinct(5) BDD.inject BDD_select.select_def erase_disj_df_noop erase_disj_disj_free erase_disjoints.simps(1) leI less_trans nat_neq_iff pwf_is_norm pwf_weaken pwfbdd.cases)
-  (* Case 3 / 5 *)
-  apply (case_tac "tr ie c")
-  apply (auto simp add: select'_def split: option.split)[3]
-  apply (metis BDD.inject BDD.simps(5) BDD.simps(7) sub_free.simps)
-  apply (metis BDD.inject BDD.simps(5) BDD.simps(7) BDD_select.select_def erase_disjoints.simps(1) less_Suc_eq_le pwf_weaken pwfbdd.cases sub_free.simps)
-  apply (metis BDD.inject BDD.simps(5) BDD.simps(7) BDD_select.select_def erase_disjoints.simps(1) less_Suc_eq_le pwf_weaken pwfbdd.cases sub_free.simps)
-  (* Case 2 / 5 *)
-  apply (case_tac "tr ie c")
-  apply (auto simp add: select'_def split: option.split)[3]
-  apply (metis BDD.inject BDD.simps(5) BDD.simps(7) sub_free.simps)
-  apply (metis BDD.inject BDD.simps(5) BDD.simps(7) BDD_select.select_def erase_disjoints.simps(1) less_Suc_eq_le pwf_weaken pwfbdd.cases sub_free.simps)
-  apply (metis BDD.inject BDD.simps(5) BDD.simps(7) BDD_select.select_def erase_disjoints.simps(1) less_Suc_eq_le pwf_weaken pwfbdd.cases sub_free.simps)
-  (* Case 1 / 5 *)
+theorem common_complete:
+  "well_formed_tr c tr \<Longrightarrow> n \<le> c \<Longrightarrow>
+   pwfbdd n tr t \<Longrightarrow> pwfbdd n tr e \<Longrightarrow> pwfbdd n tr s \<Longrightarrow>
+   t = erase_disjoints c tr s \<Longrightarrow> e = erase_subtypes c tr s \<Longrightarrow>
+   common n c tr t e \<noteq> None"
+  apply (rule common_complete_a)
+  apply (rule erase_erasures)
   apply auto
-  (* Case 12/12 *)
-  apply (case_tac "common n c tr tt te")
-  apply simp_all
-  apply (subgoal_tac "erase_disjoints c tr a = tt")
-  prefer 2
-  apply (metis BDD.inject BDD.simps(5) BDD.simps(7) disj_free.cases less_Suc_eq_le pwf_weaken pwfbdd.cases sub_free.cases)
-  apply (case_tac "common n c tr et ee")
-  apply simp_all
-  apply (subgoal_tac "erase_disjoints c tr aa = et")
-  prefer 2
-  apply (metis BDD.inject BDD.simps(5) BDD.simps(7) disj_free.cases less_Suc_eq_le pwf_weaken pwfbdd.cases sub_free.cases)
-  apply simp_all
-  apply (metis BDD.distinct(3) BDD.distinct(5) BDD.inject BDD_select.select_def disj_free.cases erase_disjoints.simps(1) pwfbdd.cases)
-  (* Case 11/12 *)
-  apply (case_tac "tr it ie")
-  apply simp_all
-  apply (case_tac "tr ie c")
-  apply simp_all
-  apply (metis BDD.inject BDD.simps(5) BDD.simps(7) sub_free.simps)
-  apply (smt (verit) BDD.distinct(3) BDD.distinct(5) BDD.inject disj_free.cases leI order.asym pwfbdd.cases wftr_refl0 wftr_sub_disj)
-  apply (case_tac "common n c tr
-              (select it (erase_disjoints ie tr tt)
-                (erase_disjoints ie tr et))
-              te")
-  apply simp_all
-  apply (case_tac "common n c tr (erase_subtypes ie tr et) ee")
-  apply simp_all
-  apply (subgoal_tac "erase_disjoints c tr a =
-        select it (erase_disjoints ie tr tt)
-         (erase_disjoints ie tr et)")
-  prefer 2
-  apply (smt (verit) BDD.distinct(3) BDD.distinct(5) BDD.inject disj_free.simps erase_disj_df_preserve erase_disj_sf_preserve erase_disjoints_pwf less_SucE less_Suc_eq_le not_le pwf_weaken pwfbdd.simps select_df_preserve select_pwf sub_free.cases)
-  apply (subgoal_tac "erase_disjoints c tr aa =
-        erase_subtypes ie tr et")
-  prefer 2
-  apply (smt (verit) BDD.distinct(3) BDD.distinct(5) BDD.inject disj_free.simps erase_disj_df_preserve erase_disj_sf_preserve erase_disjoints_pwf less_SucE less_Suc_eq_le not_le pwf_weaken pwfbdd.simps select_df_preserve select_pwf sub_free.cases)
-  apply (case_tac "tr it c")
-  apply simp_all
-  sledgehammer
-  apply (smt (verit) BDD.distinct(3) BDD.distinct(5) BDD.inject disj_free.cases leI order.asym pwfbdd.cases wftr_refl0 wftr_sub_disj)
-  (* Subcase 3 / 3 *)
-  apply (case_tac "ie < it")
-  apply (simp add: select'_def split: option.split)
-  apply auto
-  apply (case_tac "common n c tr et (Select ie te ee)")
-  apply simp_all
-  apply (case_tac "erase_disjoints c tr a = et")
-  prefer 2
-  apply (metis BDD.distinct(3) BDD.distinct(5) BDD.inject disj_free.cases less_Suc_eq_le pwf_weaken pwfbdd.simps)
-  apply (case_tac "common n c tr (Select it tt et) ee")
-  apply simp_all
-  apply (case_tac "common n c tr tt te")
-  apply simp_all
-  apply (case_tac "tr ie c")
-  apply (metis BDD.inject BDD.simps(5) BDD.simps(7) sub_free.simps)
-  apply simp_all
-  apply (smt (verit) BDD.distinct(3) BDD.distinct(5) BDD.inject BDD_select.select_def disj_free.simps erase_disj_df_noop erase_disjoints.elims le_eq_less_or_eq less_Suc_eq_le less_le_not_le pwf_is_norm pwf_weaken pwfbdd.simps)
-  apply (case_tac "tr it ie")
-  apply (simp add: select'_def split: option.split)
-  apply (case_tac "common n c tr
-         (select it (erase_disjoints ie tr tt)
-           (erase_disjoints ie tr et))
-         te")
-  apply (simp add: select_def)
-  apply (metis BDD.inject BDD.simps(5) BDD.simps(7) disj_free.cases erase_disj_df_noop less_le pwf_is_norm pwf_weaken pwfbdd.cases)
-  apply simp_all
-  apply (case_tac "common n c tr (erase_subtypes ie tr et) ee")
-  apply (simp add: select_def)
-  apply (metis BDD.distinct(3) BDD.distinct(5) BDD.inject disj_free.cases erase_disj_df_noop leD le_cases pwf_is_norm pwf_weaken pwfbdd.simps)
-  apply (simp add: select_def)
-  apply (metis BDD.distinct(3) BDD.distinct(5) BDD.inject disj_free.cases erase_disj_df_noop leD le_cases pwf_is_norm pwf_weaken pwfbdd.simps)
-  apply (case_tac "select' ie
-         (common n c tr (erase_disjoints ie tr et) te)
-         (common n c tr
-           (select it (erase_subtypes ie tr tt)
-             (erase_subtypes ie tr et))
-           ee)")
-  apply (simp add: select_def)
-  apply (metis BDD.distinct(3) BDD.distinct(5) BDD.inject disj_free.cases erase_disj_df_noop leD le_cases pwf_is_norm pwf_weaken pwfbdd.simps)
-  apply (simp add: select_def)
-  apply (metis BDD.distinct(3) BDD.distinct(5) BDD.inject disj_free.cases erase_disj_df_noop leD le_cases pwf_is_norm pwf_weaken pwfbdd.simps)
-  apply (case_tac "select' ie
-         (common n c tr
-           (select it (erase_disjoints ie tr tt)
-             (erase_disjoints ie tr et))
-           te)
-         (common n c tr
-           (select it (erase_subtypes ie tr tt)
-             (erase_subtypes ie tr et))
-           ee)")
-  apply (simp add: select_def)
-  apply (metis BDD.distinct(3) BDD.distinct(5) BDD.inject disj_free.cases erase_disj_df_noop leD le_cases pwf_is_norm pwf_weaken pwfbdd.simps)
-  apply (simp add: select_def)
-  apply (metis BDD.distinct(3) BDD.distinct(5) BDD.inject disj_free.cases erase_disj_df_noop leD le_cases pwf_is_norm pwf_weaken pwfbdd.simps)
-  apply (case_tac "common n c tr et ee")
-  apply simp_all
-  apply (case_tac "tr ie c")
-  apply simp_all
-  apply (metis BDD.distinct(3) BDD.distinct(5) BDD.inject sub_free.cases)
-  apply (simp add: select_def)
-  apply (metis BDD.distinct(3) BDD.distinct(5) BDD.inject disj_free.cases erase_disj_df_noop leD le_cases pwf_is_norm pwf_weaken pwfbdd.simps)
-  apply (case_tac "tr it ie")
-  apply simp_all
-  apply (case_tac "select' ie
-         (common n c tr
-           (select it (erase_disjoints ie tr tt)
-             (erase_disjoints ie tr et))
-           te)
-         (common n c tr (erase_subtypes ie tr et) ee)")
-  apply simp_all
-  apply (simp add: select_def)
-  apply (metis BDD.distinct(3) BDD.distinct(5) BDD.inject disj_free.cases erase_disj_df_noop leD le_cases pwf_is_norm pwf_weaken pwfbdd.simps)
-  apply (simp add: select_def)
-  apply (metis BDD.distinct(3) BDD.distinct(5) BDD.inject disj_free.cases erase_disj_df_noop leD le_cases pwf_is_norm pwf_weaken pwfbdd.simps)
-  apply (case_tac "select' ie
-         (common n c tr (erase_disjoints ie tr et) te)
-         (common n c tr
-           (select it (erase_subtypes ie tr tt)
-             (erase_subtypes ie tr et))
-           ee)")
-  apply simp_all
-  apply (simp add: select_def)
-  apply (metis BDD.distinct(3) BDD.distinct(5) BDD.inject disj_free.cases erase_disj_df_noop leD le_cases pwf_is_norm pwf_weaken pwfbdd.simps)
-  apply (simp add: select_def)
-  apply (metis BDD.distinct(3) BDD.distinct(5) BDD.inject disj_free.cases erase_disj_df_noop leD le_cases pwf_is_norm pwf_weaken pwfbdd.simps)
-  apply (case_tac "select' ie
-         (common n c tr
-           (select it (erase_disjoints ie tr tt)
-             (erase_disjoints ie tr et))
-           te)
-         (common n c tr
-           (select it (erase_subtypes ie tr tt)
-             (erase_subtypes ie tr et))
-           ee)")
-  apply simp_all
-  apply (simp add: select_def)
-  apply (metis BDD.distinct(3) BDD.distinct(5) BDD.inject disj_free.cases erase_disj_df_noop leD le_cases pwf_is_norm pwf_weaken pwfbdd.simps)
-  apply (simp add: select_def)
-  apply (metis BDD.distinct(3) BDD.distinct(5) BDD.inject disj_free.cases erase_disj_df_noop leD le_cases pwf_is_norm pwf_weaken pwfbdd.simps)
-
-(* IMPOSSIBLE *)
-  sledgehammer
-  oops
-
-
-(* FIXME: \<not>(\<exists>b. wfbdd i tr b \<and> erase_disjoints i tr b = t \<and> erase_subtypes i tr b = e) \<Longrightarrow>
-*)
-inductive wfbdd :: "nat \<Rightarrow> TR \<Rightarrow> BDD \<Rightarrow> bool" where
-  wfbdd_sel: "n > i \<Longrightarrow> sub_free i tr e \<Longrightarrow> disj_free i tr t \<Longrightarrow> 
-              wfbdd i tr t \<Longrightarrow> wfbdd i tr e \<Longrightarrow> 
-              wfbdd n tr (Select i t e)" |
-  wfbdd_top: "wfbdd n tr Top" | wfbdd_nothing: "wfbdd n tr Nothing"
-
-
+  apply (rule erase_disj_disj_free)
+  apply (rule erase_subtypes_sub_free)
+  done
+  
 
 end
